8a41d8b82e4d16a1e59bafdea0d8dab7e615b0e8
陆翔宇 <2422350954@qq.com>
2023-03-14 21:08:18 +0800

    获取openid

--- pages/pay/index.js
+++ pages/pay/index.js
@@ -2,6 +2,7 @@
 import {
   getBaseUrl,
   getWxLogin,
+  //getUserMsg,
   requestUtil,
 } from '../../utils/requestUtil';
 import regeneratorRuntime from '../../lib/runtime/runtime';
@@ -19,8 +20,9 @@ Page({
     cart: [],
     totalPrice: 0,
     totalNum: 0,
-    avatarUrl: defaultAvatarUrl,   //用户头像地址
-    nickName:'',
+    avatarUrl: defaultAvatarUrl, //用户头像地址
+    nickName: '',
+    UserMsg: [],
   },
 
 
@@ -37,36 +39,92 @@ Page({
   //载入用户头像
   onChooseAvatar(e) {
     console.log(e);
-    const { avatarUrl } = e.detail 
+    const {
+      avatarUrl
+    } = e.detail
     this.setData({
       avatarUrl,
     })
   },
 
   //将用户名载入页面数据
-  formSubmit(e){
+  formSubmit(e) {
     //console.log('昵称：',e.detail.value.nickname)
-    const  nickName  = e.detail.value.nickname
+    const nickName = e.detail.value.nickname
     //console.log(nickName);
     this.setData({
       nickName
     })
-  }, 
+  },
+
+  //用户头像、昵称封装
+  getAjax2() {
+    return new Promise((resolve, reject) => {
+      let url = this.data.avatarUrl;
+      let name = this.data.nickName;
+      let UserMsg = [];
+      UserMsg.push(url);
+      UserMsg.push(name);
+      this.setData({
+        UserMsg,
+      })
+      resolve(this.data.UserMsg);
+      reject('失败');
+    });
+  },
+
 
   //处理订单支付
-  async handleOrderPay(){
+  async handleOrderPay() {
     // wx.login({
     //   timeout:5000,
     //   success: (res) => {
     //     console.log(res.code);
     //   },
     // })
-    let res = await getWxLogin();
-    console.log(res.code);
-    console.log(this.data.avatarUrl);
-    console.log(this.data.nickName);
-   
+    // let res = await getWxLogin();
+    // console.log(res.code);
+    // console.log(this.data.avatarUrl);
+    // console.log(this.data.nickName);
 
+    const token = wx.getStorageSync('token');
+    if (!token) {
+      let p2 = this.getAjax2();
+      Promise.all([getWxLogin(), p2]).then((res) => {
+        console.log("code是  " + res[0].code);
+        console.log("头像地址  " + res[1][0]);
+        console.log("昵称  " + res[1][1]);
+        let loginParam = {
+          code: res[0].code,
+          nickName: res[1][1],
+          avatarUrl: res[1][0],
+        }
+        console.log(loginParam);
+        wx.setStorageSync('userInfo', res[1]);
+        this.wxlogin(loginParam);
+      })
+    }else{
+      console.log("token存在："+ token);
+    }
+
+
+  },
+
+  //请求后端获取用户token
+  async wxlogin(loginParam) {
+    const result = await requestUtil({
+      url: "/user/wxlogin",
+      data: loginParam,
+      method: "post"
+    })
+    console.log(result);
+    const token = result.token;
+    if (result.code === 0) {
+      //存储token到缓存
+      wx.setStorageSync('token', token);
+      //继续创建订单
+      console.log("支付继续，创建订单");
+    }
   },
 
   /**
@@ -75,7 +133,7 @@ Page({
   onShow() {
     const address = wx.getStorageSync('address');
     let cart = wx.getStorageSync('cart') || [];
-    cart = cart.filter(v=>v.checked);     //购物车过滤，只要已选中的商品
+    cart = cart.filter(v => v.checked); //购物车过滤，只要已选中的商品
     let totalPrice = 0;
     let totalNum = 0;
     cart.forEach(v => { //遍历购物车中每一条商品
--- utils/requestUtil.js
+++ utils/requestUtil.js
@@ -1,6 +1,6 @@
 //定义请求根路径baseUrl
 //const baseUrl ="http://localhost:8080";         
-const baseUrl ="http://10.12.57.113:8080";          //BUU
+const baseUrl = "http://10.12.57.113:8080"; //BUU
 //const baseUrl ="http://192.168.43.163:8080";      //手机wifi
 //const baseUrl ="http://192.168.16.122:8080";       //晶晶哥家
 
@@ -8,19 +8,19 @@ const baseUrl ="http://10.12.57.113:8080";          //BUU
 let ajaxTimes = 0;
 
 //返回请求根路径baseUrl
-export const getBaseUrl=()=>{
+export const getBaseUrl = () => {
   return baseUrl;
 }
 
 //wx.login封装
-export const getWxLogin=()=>{
-  return new Promise((resolve,reject)=>{
+export const getWxLogin = () => {
+  return new Promise((resolve, reject) => {
     wx.login({
-      timeout:5000,
+      timeout: 5000,
       success: (res) => {
         resolve(res);
       },
-      fail:(err)=>{
+      fail: (err) => {
         reject(err);
       }
     })
@@ -28,52 +28,53 @@ export const getWxLogin=()=>{
 }
 
 //用户头像、昵称封装
-export const getUserMsg=()=>{
-  return new Promise((resolve,reject)=>{
-    wx.login({
-      timeout:5000,
-      success: (res) => {
-        resolve(res);
-      },
-      fail:(err)=>{
-        reject(err);
-      }
-    })
-  });
-}
+// export const getUserMsg = () => {
+//   return new Promise((resolve, reject) => {
+//     let url = this.data.avatarUrl;
+//     let name = this.data.nickName;
+//     let UserMsg = [];
+//     UserMsg.push(url, name);
+//     this.setData({
+//       UserMsg,
+//     })
+//     resolve(this.data.UserMsg);
+//     reject('失败');
+//   });
+// }
+
 
 //后端请求工具类
-export const requestUtil = (params)=>{
+export const requestUtil = (params) => {
 
   var start = new Date().getTime();
   console.log(start);
 
 
-  ajaxTimes++;          //每次请求时，ajaxTimes加一
+  ajaxTimes++; //每次请求时，ajaxTimes加一
   wx.showLoading({
     title: '加载中',
-    mask:true,
+    mask: true,
   })
 
   //模拟网络延迟加载
-  while(true){
-    if(new Date().getTime()-start>=1*100) break;
+  while (true) {
+    if (new Date().getTime() - start >= 1 * 100) break;
   }
 
-  return new Promise((resolve,reject)=>{
+  return new Promise((resolve, reject) => {
     wx.request({
       ...params,
-      url : baseUrl+params.url,
-      success:(result)=>{
+      url: baseUrl + params.url,
+      success: (result) => {
         resolve(result.data)
       },
-      fail:(err)=>{
+      fail: (err) => {
         reject(err)
       },
-      complete:()=>{
-        ajaxTimes--;            //每次请求完成，ajaxTimes减一
-        if(ajaxTimes==0){       //避免页面多个请求时出错
-          wx.hideLoading();     //关闭加载图标
+      complete: () => {
+        ajaxTimes--; //每次请求完成，ajaxTimes减一
+        if (ajaxTimes == 0) { //避免页面多个请求时出错
+          wx.hideLoading(); //关闭加载图标
         }
       }
     })
